fb5d306dd57d68219e15e620f0b4094b69263565
groeluke <groeluke@isu.edu>
2026-09-24 09:23:26 -0600

    add a message box to say I am going to throw the dart

--- DartGameFall26/DartGame.Designer.cs
+++ DartGameFall26/DartGame.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            ExitButton = new Button();
+            DrawPictureBox = new PictureBox();
+            ThrowDartButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)DrawPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(625, 380);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(163, 58);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // DrawPictureBox
+            // 
+            DrawPictureBox.Location = new Point(36, 24);
+            DrawPictureBox.Name = "DrawPictureBox";
+            DrawPictureBox.Size = new Size(736, 340);
+            DrawPictureBox.TabIndex = 1;
+            DrawPictureBox.TabStop = false;
+            // 
+            // ThrowDartButton
+            // 
+            ThrowDartButton.Location = new Point(456, 380);
+            ThrowDartButton.Name = "ThrowDartButton";
+            ThrowDartButton.Size = new Size(163, 58);
+            ThrowDartButton.TabIndex = 2;
+            ThrowDartButton.Text = "Throw &Dart";
+            ThrowDartButton.UseVisualStyleBackColor = true;
+            ThrowDartButton.Click += ThrowDartButton_Click;
+            // 
+            // DartGame
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(ThrowDartButton);
+            Controls.Add(DrawPictureBox);
+            Controls.Add(ExitButton);
+            Name = "DartGame";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)DrawPictureBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button ExitButton;
+        private PictureBox DrawPictureBox;
+        private Button ThrowDartButton;
     }
 }
--- DartGameFall26/DartGame.cs
+++ DartGameFall26/DartGame.cs
@@ -1,10 +1,34 @@
 namespace DartGameFall26
+/*Luke Groesbeck
+*Fall 2026
+*RCET 3371
+*DartGame
+*Advanced Programming Techniques & GUI Development
+*https://github.com/groeluke/DartGameF26
+*/
 {
     public partial class DartGame : Form
     {
         public DartGame()
         {
             InitializeComponent();
+        }
+        void DrawDart()
+        {
+            MessageBox.Show("Ima throw a dart now!");
+
+        }
+
+        //Event handlers below this***************************************
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ThrowDartButton_Click(object sender, EventArgs e)
+        {
+            DrawDart();
         }
     }
 }
